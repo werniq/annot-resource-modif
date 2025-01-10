d04c721becf8e971c34a16586e1852a64deb8638
werniq <qniwwwersss@gmail.com>
2025-01-10 11:45:10 +0100

    feat: Added updateErrorStatus method, added more documentation

--- operator/internal/controller/resourcemodifier_controller_status_updates.go
+++ operator/internal/controller/resourcemodifier_controller_status_updates.go
@@ -11,6 +11,22 @@ func (r *ResourceModifierReconciler) initResourceModifierStatus(resource v1.Reso
 	resource.Status.Conditions = make(map[string]string)
 }
 
+// updateErrorStatus updates resource's Conditions with appropriate message. If an error were returned, returns it.
+func (r *ResourceModifierReconciler) updateErrorStatus(resource v1.ResourceModifier, reason string) (ctrl.Result, error) {
+	resource.Status.ErrorStatus(reason)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	err := r.Client.Update(ctx, &resource)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// updateStatusSuccess updates resource's Conditions by adding new Successful status, and removing any previously added
+// error statuses (if applicable).
 func (r *ResourceModifierReconciler) updateStatusSuccess(resource v1.ResourceModifier, reason string) (ctrl.Result, error) {
 	resource.Status.SuccessfulStatus(reason)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
