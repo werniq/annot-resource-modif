fea6bfa94af0e78663a40476057230a0373d6f8e
werniq <qniwwwersss@gmail.com>
2025-01-10 11:40:53 +0100

    feat: Added updating resource after adding new status

--- operator/internal/controller/resourcemodifier_controller_status_updates.go
+++ operator/internal/controller/resourcemodifier_controller_status_updates.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"context"
 	v1 "ericsson.com/resource-modif-annotations/api/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"time"
 )
 
 func (r *ResourceModifierReconciler) initResourceModifierStatus(resource v1.ResourceModifier) {
@@ -10,5 +12,14 @@ func (r *ResourceModifierReconciler) initResourceModifierStatus(resource v1.Reso
 }
 
 func (r *ResourceModifierReconciler) updateStatusSuccess(resource v1.ResourceModifier, reason string) (ctrl.Result, error) {
+	resource.Status.SuccessfulStatus(reason)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	err := r.Client.Update(ctx, &resource)
+	if err != nil {
+
+	}
+
 	return ctrl.Result{}, nil
 }
