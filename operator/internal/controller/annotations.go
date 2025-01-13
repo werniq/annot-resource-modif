c0b81f1766f5a15c62b25c55ce412bedbad40349
werniq <qniwwwersss@gmail.com>
2025-01-13 10:45:49 +0100

    feat: Implemented Scale annotation

--- operator/internal/controller/annotations.go
+++ operator/internal/controller/annotations.go
@@ -3,6 +3,10 @@ package controller
 import (
 	"context"
 	annotresourcemodifv1 "ericsson.com/resource-modif-annotations/api/v1"
+	errs "errors"
+	v1 "k8s.io/api/apps/v1"
+	v2 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/remotecommand"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"slices"
 	"strings"
@@ -18,6 +22,12 @@ const (
 
 	// successAddLabel
 	successAddLabel = "Successfully added label"
+
+	// successDeploymentScale
+	successDeploymentScale = "Successfully scaled deployment"
+
+	// errTypeNotScalable
+	errTypeNotScalable = "given Resource Type is not scalable"
 )
 
 // executeRemoveAnyFinalizerAnnotation
@@ -137,6 +147,53 @@ func (r *ResourceModifierReconciler) executeRemoveLabel(resource client.Object,
 		if updateErr != nil {
 			return updateErr
 		}
+		return err
+	}
+
+	return nil
+}
+
+// executeScale
+func (r *ResourceModifierReconciler) executeScale(resource client.Object,
+	rm annotresourcemodifv1.ResourceModifier, desiredReplicas int) error {
+
+	var deployment *v1.Deployment
+	var ok bool
+	deployment, ok = resource.(*v1.Deployment)
+	if !ok {
+		return errs.New(errTypeNotScalable)
+	}
+
+	err := r.scaleDeployment(deployment, desiredReplicas)
+	if err != nil {
+		return err
+	}
+
+	err = r.updateStatusSuccess(rm, successDeploymentScale)
+	if err != nil {
+		updateErr := r.updateErrorStatus(rm, err.Error())
+		if updateErr != nil {
+			return updateErr
+		}
+		return err
+	}
+
+	return nil
+}
+
+// scaleDeployment updates Spec.Replicas in the specified deployment.
+func (r *ResourceModifierReconciler) scaleDeployment(d *v1.Deployment, desiredReplicas int) error {
+	var replicas *int32
+	replicas = new(int32)
+	*replicas = int32(desiredReplicas)
+
+	d.Spec.Replicas = replicas
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	err := r.Client.Update(ctx, d)
+	if err != nil {
 		return err
 	}
 
